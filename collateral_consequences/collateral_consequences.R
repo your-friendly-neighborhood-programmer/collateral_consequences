e092ababf75561f9fe0eaf88d66d5cde81ac4a6c
Victoria Mitchell <victoriagrace@usa.com>
2024-12-01 12:14:16 -0600

    Update collateral_consequences.R

--- collateral_consequences/collateral_consequences.R
+++ collateral_consequences/collateral_consequences.R
@@ -127,7 +127,7 @@ all_states <- rbind(alabama, alaska, arizona, arkansas, california,
 # Sum of entries analyzed, total collateral consequences
 no_con <- all_states$Number.of.Consequences
 num_con <- as.integer(no_con)
-clean_num_con <- sum(num_con, na.rm = TRUE)
+clean_total_consequences <- sum(num_con, na.rm = TRUE)
 
 # Count number of collateral consequences per jurisdiction/state
 n_consequences <- data.frame(all_states %>% group_by(state) %>% summarise(count = n()))
